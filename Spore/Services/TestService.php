08b5f11850217f6475cc570fe136949404e79dfe
Danny Kopping <dannykopping@gmail.com>
2012-10-06 01:06:51 +0200

    Added a few usage examples

--- Spore/Services/TestService.php
+++ Spore/Services/TestService.php
@@ -11,12 +11,87 @@
 	class TestService
 	{
 		/**
-		 * @url			/hi/:bob
-		 * @verbs		GET,POST
-		 * @auth		bob
+		 * @url			/example1
+		 * @verbs		GET
 		 */
-		public function hi(Request $req, Response $res)
+		public function example1(Request $request, Response $response)
 		{
-			return ($req|1);
+			return array("some" => "complex", "data" => "in an array",
+						 	"with" => array("nesting"));
+		}
+
+		/**
+		 * @url			/example2
+		 * @verbs		GET
+		 */
+		public function example2(Request $request, Response $response)
+		{
+			// set a status code
+			$response->status = Status::CREATED;
+
+			return true;
+		}
+
+		/**
+		 * @url			/example3/:param1+:param2
+		 * @verbs		POST
+		 */
+		public function example3(Request $request, Response $response)
+		{
+			// use named request params
+			return ((int) $request->params["param1"]) + ((int) $request->params["param2"]);
+		}
+
+		/**
+		 * @url			/example4/:name
+		 * @verbs		POST
+		 */
+		public function example4(Request $request, Response $response)
+		{
+			// named parameters as defined in URL with ":" prefix
+			$params = $request->params;
+
+			// parameters passed in as a query string, e.g. /url?greeting=hello
+			$queryStrParams = $request->queryParams;
+
+			// data passed along in body of HTTP request, and deserialized according to content-type
+			$bodyParams = $request->data;
+
+			return array($params, $queryStrParams, $bodyParams);
+		}
+
+		/**
+		 * @url			/example5
+		 * @verbs		GET
+		 */
+		public function example5(Request $request, Response $response)
+		{
+			// return complex objects
+			return $request;
+		}
+
+		/**
+		 * @url			/example6
+		 * @verbs		GET
+		 * @auth		restricted
+		 */
+		public function example6()
+		{
+			// add an @auth annotation to restrict access
+			// see Spore::setAuthCallback
+
+			return "you made it!";
+		}
+
+		/**
+		 * @url			/example7
+		 * @verbs		GET
+		 */
+		public function example7(Request $request, Response $response)
+		{
+			// manually set an HTTP header
+			$response->headers["Look Ma"] = "No hands!";
+
+			return "Look at the header!";
 		}
 	}
