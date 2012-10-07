4cde94e542831d1b11760c35f8daaa6b093783f4
Danny Kopping <dannykopping@gmail.com>
2012-10-07 20:15:31 +0200

    Removed Twig-specific initialization code

--- index.php
+++ index.php
@@ -2,17 +2,8 @@
 	require_once "vendor/autoload.php";
 
 	use Spore\Spore;
-	use Slim\Extras\Views\Twig;
 
-	$twigView             = new \Slim\Extras\Views\Twig();
-	Twig::$twigExtensions = array(
-		'Twig_Extensions_Slim',
-	);
-
-	$app = new Spore(array(
-						  'view' => $twigView
-					 ));
-
+	$app = new Spore();
 	$app->get("/", function ()
 	{
 		return array("message" => "Hello World from Spore");
