758e5ad7d9929b50fddda80477bc11afec4a9a10
mathielen <mathielen@gmail.com>
2013-02-23 11:05:29 +0100

    Update Spore/ReST/Data/Serializer/JSONSerializer.php
    What do you think about that extension?
    Should be implemented in XmlSerializer, too.

--- Spore/ReST/Data/Serializer/JSONSerializer.php
+++ Spore/ReST/Data/Serializer/JSONSerializer.php
@@ -7,6 +7,10 @@ class JSONSerializer extends Base
 {
     public static function parse($data)
     {
+        if (is_object($data) && method_exists($data, 'toArray')) {
+    		$data = $data->toArray();
+    	}
+        
         if (function_exists("json_encode")) {
             return json_encode($data);
         } else {
